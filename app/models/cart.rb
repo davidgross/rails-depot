e795043da0f5d68920f1cd7724de4441bd7be5fe
David Gross <david.gross@daggerweb.org>
2014-08-19 13:05:44 -0600

    Cart created
    setup relationships between carts and line-items,
    created a private method in a concern.
    
    ch9 complete

---
+++ app/models/cart.rb
@@ -0,0 +1,16 @@
+class Cart < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_Items.emty?
+        return true
+      else
+        errors.add(:base, 'Line Items Present')
+        return false
+      end
+    end
+end
