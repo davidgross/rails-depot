643287ffe6581f45aaa6b8becdf7bbdb704ff055
David Gross <david.gross@daggerweb.org>
2014-08-19 13:18:11 -0600

    added session visit count to store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @visit_count = store_visits
+  end
+
+  def store_visits
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
