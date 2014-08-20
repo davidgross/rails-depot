436ae060cd290cb9dfcc8174871e6670e058709b
David Gross <david.gross@daggerweb.org>
2014-08-20 15:19:20 -0600

    I18n'd it
    Added spanish i18n to the application.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,8 +3,12 @@ class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
   def index
-    @products = Product.order(:title)
-    @visit_count = store_visits
+    if params[:set_locale]
+      redirect_to store_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @visit_count = store_visits
+    end
   end
 
   def store_visits
