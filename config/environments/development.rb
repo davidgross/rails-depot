2996a021a1d728b14dd3cb1d8d615d07cc4aa3b0
David Gross <david.gross@daggerweb.org>
2014-08-20 13:17:50 -0600

    Added mailer
    Added mailer functionality, and related test, unittests, and intigration
    tests.
    
    This completes ch13

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #disable smtp
+  config.action_mailer.delivery_method = :test
 end
